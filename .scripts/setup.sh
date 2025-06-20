dcee5f6916f208fe43be0dd2f9faa423a7baae35
Leo Pitt <leopitt@function-design.co.uk>
2025-06-20 13:05:02 +0100

    Tidying up repo and gitignore.

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Start DDEV.
 ddev start
+# Initialise dependencies.
+ddev composer install --no-interaction
 # Initialise the database.
-gunzip -c .init/init.sql.gz | ddev drush sqlq
+gunzip -c .init/init.sql.gz | ddev mysql
 # Initialise files.
 rm -rf web/sites/default/files
 tar -xzf .init/files.tar.gz web/sites/default/files
